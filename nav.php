47081a977b3c2737f14cb2e999955b95927f9003
Mariia Posvystak <posvystakmariia@gmail.com>
2025-11-25 11:30:28 +0200

    Uuendamine nav

--- nav.php
+++ nav.php
@@ -4,8 +4,12 @@
         <li><a href="?link=gitKasutamine.php">Git käsud</a></li>
         <li><a href="?link=muusikaKysitlus.php">JS: Muusika küsitlus</a></li>
         <li><a href="?link=ValuutKalkulaator.php">JS: Random pilti näitamine ja valuutikalkulaator</a></li>
-        <li><a href="?link=Ajafunktsioon.php">Ajafunktsioonid</a></li>
-        <li><a href="?link=matem_funkts.php">Matemaatiliste funktsioonid</a></li>
+        <li><a href="#">Funktsioonid</a>
+            <ul class="dropdown">
+                <li><a href="?link=Ajafunktsioon.php">Ajafunktsioonid</a></li>
+                <li><a href="?link=matem_funkts.php">Matemaatiliste funktsioonid</a></li>
+            </ul>
+        </li>
         <li><strong><a href="https://mariiaposvystak24.thkit.ee/" target="_blank">Vana index</a></strong></li>
 
     </ul>
