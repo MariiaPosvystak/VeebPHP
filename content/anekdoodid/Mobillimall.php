7e92b36c049348ba470ad2e3648150c737340814
Mariia Posvystak <posvystakmariia@gmail.com>
2025-11-27 17:01:05 +0200

    Mobillimall on loodatud

--- content/anekdoodid/Mobillimall.php
+++ content/anekdoodid/Mobillimall.php
@@ -1,1 +1,18 @@
 <?php
+echo "<div class='uuscontainer'>";
+
+echo "<h1>Mobillimalli järgi veebilehestik</h1>";
+echo "<div>";
+echo "<h2>Sissejuhatus</h2>";
+
+echo "</div>";
+echo "<div>";
+echo "<h2>Koodilõigud</h2>";
+
+echo "</div>";
+echo "<div>";
+echo "<h2>Mobiilivaade</h2>";
+
+echo "</div>";
+echo "</div>";
+?>
